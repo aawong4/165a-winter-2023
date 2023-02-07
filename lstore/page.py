ef4e72ec46832d98f57ec6bd4581f8e63aa278c9
Aaron Wong <aaronwong@Aarons-MacBook-Pro.local>
2023-02-07 14:34:56 -0800

    Update to page.py

--- lstore/page.py
+++ lstore/page.py
@@ -1,3 +1,5 @@
+# Page class: Page has 4096 bytes, each record is 8 bytes
+# Total: 512 records per page
 
 class Page:
 
@@ -5,10 +7,16 @@ class Page:
         self.num_records = 0
         self.data = bytearray(4096)
 
+    # Returns if the page has space for a new record
     def has_capacity(self):
-        pass
-
+        return self.num_records < 512
+        
+    # Writes value to next available record
     def write(self, value):
+        write_offset = self.num_records * 8
+        self.data[write_offset : write_offset + 8] = value.to_bytes(8, 'big')
         self.num_records += 1
-        pass
 
+    # Returns data of page
+    def read(self):
+        return self.data
