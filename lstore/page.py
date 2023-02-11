b09f7cd1b78b408033f3698278c1c67af073fb04
Aaron Wong <97322392+aawong4@users.noreply.github.com>
2023-02-11 14:15:55 -0800

    Update page.py

--- lstore/page.py
+++ lstore/page.py
@@ -1,59 +1,29 @@
+import lstore.config
+
 # Page class: Page has 4096 bytes, each record is 8 bytes
 # Total: 512 records per page
-
-class Page:
+class Physical_Page:
 
     def __init__(self):
         self.num_records = 0
-        self.data = bytearray(4096)
+        self.data = bytearray(lstore.config.BYTES_PER_PAGE)
 
     # Returns if the page has space for a new record
     def has_capacity(self):
-        return self.num_records < 512
-        
-    # Writes value to next available record
-    def write(self, value):
-        write_offset = self.num_records * 8
-        self.data[write_offset : write_offset + 8] = value.to_bytes(8, 'big')
+        return self.num_records < lstore.config.BYTES_PER_PAGE / lstore.config.BYTES_PER_ENTRY
+
+    # Writes to the entry indicated by the offset
+    def write(self, offset, value):
+        next_entry = offset * lstore.config.BYTES_PER_ENTRY
+        self.data[next_entry: next_entry + lstore.config.BYTES_PER_ENTRY] = value.to_bytes(lstore.config.BYTES_PER_ENTRY, 'big')
         self.num_records += 1
 
     # Returns data of page
-    def read(self):
-        return self.data
-
-# Modified by Kenny Sun @ 2/11/2023
-class BasePage:
-    def __init__(self, column_num):
-        self.num = column_num
-        self.columns = []
-        for i in range(column_num + 4):
-            self.columns.append(Page())
-
-
-
-class TailPage:
-    def __init__(self, column_num):
-        self.num = column_num
-        self.columns = []
-        row_num = 0
-        for i in range(column_num + 4):
-            self.columns.append(Page())
-
-
-
-class Page_Range:
-    def __init__(self, column_num):
-        self.column_num = column_num
-        self.base_page_list = [BasePage(column_num)]
-        self.tail_page_list = [TailPage(column_num)]
-    # INDIRECTION_COLUMN = 0
-    # RID_COLUMN = 1
-    # TIMESTAMP_COLUMN = 2
-    # SCHEMA_ENCODING_COLUMN = 3
-    def append(self, columns):
-        if not self.tail_page_list[-1].columns[0].has_capacity():
-            self.tail_page_list.append(TailPage(self.column_num))
-        else:
-            for i in range(len(columns)):
-                self.tail_page_list[-1].columns[i].write(columns[i])
-    pass
+    def read(self, offset):
+        row_number = self.data[offset * lstore.config.BYTES_PER_ENTRY: offset * lstore.config.BYTES_PER_ENTRY + lstore.config.BYTES_PER_ENTRY]
+        return int.from_bytes(row_number)
+    
+    # Reset the number of valid records to 0
+    #NOTE: this does not clear the actual data in the entries
+    def clean(self):
+        self.num_records = 0
